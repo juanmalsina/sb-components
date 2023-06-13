27d422d9cc7ed5507f9c7bf1eefd9f4c423697f1
Juanmalsina <juanmalsina.94@gmail.com>
2023-06-13 09:46:45 -0300

    firts commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./mylabel.css";
+
+interface MyLabelProps {
+  /*
+   *Es el texto que va en el label custom
+   */
+  label: string;
+  /**
+   * El tamaño del label
+   */
+  size?: "normal" | "h1" | "h2" | "h3";
+  /**
+   * Mayúsculas
+   */
+  allCaps?: boolean;
+  /**
+   * Colores básicos del botón
+   */
+  color?: "primary" | "secondary" | "tertiary";
+
+  /**
+   * Probar colores fuentes
+   */
+  fontColor?: string;
+}
+
+const MyLabel = ({
+  label = "No label",
+  size = "normal",
+  allCaps = false,
+  color = "primary",
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <span
+      className={`label ${size}  text-${color}`}
+      style={{ color: fontColor }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
+
+export default MyLabel;
