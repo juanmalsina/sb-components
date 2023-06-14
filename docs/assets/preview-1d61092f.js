2f95ee79d8a22c9c869c679ff0d0c9649c620c4b
Juanmalsina <juanmalsina.94@gmail.com>
2023-06-14 15:09:00 -0300

    perf: deleted docs for see page in github

--- docs/assets/preview-1d61092f.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-27d93053.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-d3d48582.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-d3d48582.js","./iframe-27d93053.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-c39e4407.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-1d61092f.js.map
